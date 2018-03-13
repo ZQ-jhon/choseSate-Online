a1f90cf61f47af23143b532a29fb84f2993fd277
zhangqiang <350037310@qq.com>
2018-03-13 22:47:40 +0800

    update

---
+++ index.js
@@ -0,0 +1,40 @@
+	<script type="text/javascript">
+	
+		
+		for(let i=0;i<900;i++){
+			//创建座位
+			var sate = document.createElement('div');
+			
+			//创建座位号，每项在序号后+1，避免“座位0”出现。
+			var text = document.createTextNode(i+1);
+			
+			sate.appendChild(text);
+			//类名
+			sate.className= 'sate';
+			
+			//css样式
+			sate.style.height =  '25px';
+			sate.style.width = '25px';
+			sate.style.background = 'gainsboro';
+			sate.style.border = '1px solid black';
+			sate.style.margin = '2px';
+			sate.style.float = 'left';
+			
+			//座位号
+			
+			//插入
+			document.body.appendChild(sate);
+		}
+		
+		
+		//事件委托
+		document.body.addEventListener('click',function(e){
+			
+			//获取当前选中的目标
+			 var target =  e.target; 
+			 
+			target.style.background = target.style.background=='gainsboro'?'yellow':'gainsboro';
+			  
+			  
+		},false);
+	</script>
